fb5beb07a9c894d7c4414e901304341b7d4b3ad7
seheraksam <05seher@gmail.com>
2022-04-20 23:54:47 +0300

    Add project files.

---
+++ VeriTabani1/Forms/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace VeriTabani1
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(151, 129);
+            this.btnLogIn.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(106, 47);
+            this.btnLogIn.TabIndex = 0;
+            this.btnLogIn.Text = "Login";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(151, 26);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(262, 30);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(151, 82);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 30);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Username.Location = new System.Drawing.Point(19, 29);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(88, 27);
+            this.Username.TabIndex = 3;
+            this.Username.Text = "E posta";
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(20, 85);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(95, 24);
+            this.Password.TabIndex = 4;
+            this.Password.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(296, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 47);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Kaydol";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 240);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnLogIn);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ VeriTabani1/Forms/Kayit.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace VeriTabani1
+{
+    partial class Kayit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKayitAd = new System.Windows.Forms.TextBox();
+            this.txtKayitEposta = new System.Windows.Forms.TextBox();
+            this.txtKayitSoyad = new System.Windows.Forms.TextBox();
+            this.txtKayitSifre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 109);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyad:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 152);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 27);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Eposta:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 195);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 27);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Şifre:";
+            // 
+            // txtKayitAd
+            // 
+            this.txtKayitAd.Location = new System.Drawing.Point(175, 59);
+            this.txtKayitAd.Name = "txtKayitAd";
+            this.txtKayitAd.Size = new System.Drawing.Size(100, 34);
+            this.txtKayitAd.TabIndex = 4;
+            // 
+            // txtKayitEposta
+            // 
+            this.txtKayitEposta.Location = new System.Drawing.Point(175, 145);
+            this.txtKayitEposta.Name = "txtKayitEposta";
+            this.txtKayitEposta.Size = new System.Drawing.Size(100, 34);
+            this.txtKayitEposta.TabIndex = 5;
+            // 
+            // txtKayitSoyad
+            // 
+            this.txtKayitSoyad.Location = new System.Drawing.Point(175, 102);
+            this.txtKayitSoyad.Name = "txtKayitSoyad";
+            this.txtKayitSoyad.Size = new System.Drawing.Size(100, 34);
+            this.txtKayitSoyad.TabIndex = 5;
+            // 
+            // txtKayitSifre
+            // 
+            this.txtKayitSifre.Location = new System.Drawing.Point(175, 188);
+            this.txtKayitSifre.Name = "txtKayitSifre";
+            this.txtKayitSifre.Size = new System.Drawing.Size(100, 34);
+            this.txtKayitSifre.TabIndex = 6;
+            // 
+            // Kayit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 420);
+            this.Controls.Add(this.txtKayitSifre);
+            this.Controls.Add(this.txtKayitSoyad);
+            this.Controls.Add(this.txtKayitEposta);
+            this.Controls.Add(this.txtKayitAd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Kayit";
+            this.Text = "Kayit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKayitAd;
+        private System.Windows.Forms.TextBox txtKayitEposta;
+        private System.Windows.Forms.TextBox txtKayitSoyad;
+        private System.Windows.Forms.TextBox txtKayitSifre;
+    }
+}
